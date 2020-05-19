a1168cd7406a5e598077fd698eef092bfc991d86
EPS <57107046+eps-uloyola@users.noreply.github.com>
2020-05-19 13:49:09 +0200

    Update service.sh

--- service.sh
+++ service.sh
@@ -6,7 +6,7 @@ case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar MyJavaOPS $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            nohup java -jar MyJavaOPS $PATH_TO_JAR  &
                         echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
